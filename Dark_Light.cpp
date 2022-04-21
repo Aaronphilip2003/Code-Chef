3ad980d74bf87192e0d39aff543af29293dde80c
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-04-21 21:51:12 +0530

    Create Dark_Light.cpp

---
+++ Dark_Light.cpp
@@ -0,0 +1,34 @@
+// PROBLEM CODE : https://www.codechef.com/problems/DARLIG
+// SOLUTION CODE : https://www.codechef.com/viewsolution/63383986  
+#include <iostream>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    ll testcases;
+    cin >> testcases;
+    while (testcases--)
+    {
+        ll N, K;
+        cin >> N >> K;
+        if (K == 1)
+        {
+            if (N % 4 == 0)
+            {
+                cout << "On" << endl;
+            }
+            else
+                cout << "Ambiguous" << endl;
+        }
+        else
+        {
+            if (N % 4 == 0)
+            {
+                cout << "Off" << endl;
+            }
+            else
+                cout << "On" << endl;
+        }
+    }
+    return 0;
+}
