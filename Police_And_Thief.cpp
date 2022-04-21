efef2777b97d4d9e75d4666bd5b54b9a94d905ed
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-04-21 21:28:13 +0530

    Create Police_And_Thief.cpp

---
+++ Police_And_Thief.cpp
@@ -0,0 +1,36 @@
+// PROBLEM CODE : https://www.codechef.com/problems/POLTHIEF
+// SOLUTION CODE : https://www.codechef.com/viewsolution/63383045
+#include <iostream>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ll testcases = 0;
+    cin >> testcases;
+    while (testcases--)
+    {
+        ll X, Y;
+        cin >> X >> Y; // 2 1
+        ll time = 0;
+        if (X > Y)
+        {
+            while (X > Y)
+            {
+                X = X - 2;
+                Y = Y - 1;
+                time++;
+            }
+        }
+        else if (X < Y)
+        {
+            while (X < Y)
+            {
+                X = X + 2;
+                Y = Y + 1;
+                time++;
+            }
+        }
+        cout << time << endl;
+    }
+    return 0;
+}
