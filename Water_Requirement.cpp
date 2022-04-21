e5029d53870532d9ce787a5846a9cbf1e59a07bf
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-04-21 17:23:40 +0530

    Create Water_Requirement.cpp

---
+++ Water_Requirement.cpp
@@ -0,0 +1,17 @@
+// PROBLEM CODE : https://www.codechef.com/problems/WATERREQ
+// SOLUTION CODE : https://www.codechef.com/viewsolution/63374107
+#include <iostream>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll N;
+        cin >> N;
+        cout << 2 * N << endl;
+    }
+    return 0;
+}
